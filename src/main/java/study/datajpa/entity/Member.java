0d76cbe4c5ccd9bf52f285579605a044d949a65e
kyuyoungoh <oqoq59@gmail.com>
2021-10-02 15:33:43 +0900

    add Member Entity

---
+++ src/main/java/study/datajpa/entity/Member.java
@@ -0,0 +1,30 @@
+package study.datajpa.entity;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Entity
+@Getter
+@Setter
+public class Member {
+
+    @Id
+    @GeneratedValue
+    @Column(name = "member_id")
+    private Long id;
+    private String userName;
+    private int age;
+
+    @ManyToOne
+    @JoinColumn(name = "team_id")
+    private Team team;
+
+    protected Member() {
+
+    }
+    public Member(String userName) {
+        this.userName = userName;
+    }
+}
