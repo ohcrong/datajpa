465fd34140e63ece7b3b3df67d1b7325dccb7e84
kyuyoungoh <oqoq59@gmail.com>
2021-10-02 15:34:06 +0900

    add Team Entity

---
+++ src/main/java/study/datajpa/entity/Team.java
@@ -0,0 +1,23 @@
+package study.datajpa.entity;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Getter
+@Setter
+public class Team {
+
+    @Id
+    @GeneratedValue
+    @Column(name = "team_id")
+    private Long id;
+    private String teamName;
+
+    @OneToMany(mappedBy = "member_id")
+    private List<Member> members = new ArrayList<>();
+}
