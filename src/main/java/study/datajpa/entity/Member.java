8f4a54639efb7fafa6db5cb39d8af037a7fa895b
kyuyoungoh <oqoq59@gmail.com>
2021-10-04 21:04:28 +0900

    Entity lombok

--- src/main/java/study/datajpa/entity/Member.java
+++ src/main/java/study/datajpa/entity/Member.java
@@ -25,4 +25,17 @@ public class Member {
     public Member(String userName) {
         this.userName = userName;
     }
+
+    public Member(String userName, int age, Team team) {
+        this.userName = userName;
+        this.age = age;
+        if (team != null) {
+            changeTeam(team);
+        }
+    }
+
+    public void changeTeam(Team team) {
+        this.team = team;
+        team.getMembers().add(this);
+    }
 }
