a98a5ce87c83ab43530ad6ecab76ffa31c5a45d0
kyuyoungoh <oqoq59@gmail.com>
2021-10-05 19:58:50 +0900

    Member Repository Test

---
+++ src/test/java/study/datajpa/entity/MemberTest.java
@@ -0,0 +1,50 @@
+package study.datajpa.entity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+@Transactional
+@Rollback(value = false)
+class MemberTest {
+
+    @PersistenceContext
+    EntityManager em;
+
+    @Test
+    public void testEntity() {
+        Team teamA = new Team("teamA");
+        Team teamB = new Team("teamB");
+        em.persist(teamA);
+        em.persist(teamB);
+
+        Member member1 = new Member("member1", 20, teamA);
+        Member member2 = new Member("member2", 20, teamB);
+        Member member3 = new Member("member3", 20, teamA);
+        Member member4 = new Member("member4", 20, teamB);
+        em.persist(member1);
+        em.persist(member2);
+        em.persist(member3);
+        em.persist(member4);
+
+        em.flush();
+        em.clear();
+
+        List<Member> results = em.createQuery("select m from Member m", Member.class)
+                .getResultList();
+        for (Member result : results) {
+            System.out.println("member -> " + result);
+            System.out.println("member.team -> " + result.getTeam());
+        }
+    }
+
+}
