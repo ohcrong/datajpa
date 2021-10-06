50a16f3c0c85cbe349c52b8029f8986cb4b0e78c
kyuyoungoh <oqoq59@gmail.com>
2021-10-06 19:59:00 +0900

    Team Repository

---
+++ src/test/java/study/datajpa/repository/TeamRepositoryTest.java
@@ -0,0 +1,31 @@
+package study.datajpa.repository;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+import study.datajpa.entity.Member;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+@Transactional
+public class TeamRepositoryTest {
+
+    @Autowired
+    MemberRepository memberRepository;
+
+    @Test
+    public void findByUsernameAndAgeGreaterThan(){
+        Member a = new Member("A", 20);
+        Member b = new Member("A", 30);
+        memberRepository.save(a);
+        memberRepository.save(b);
+
+        List<Member> members = memberRepository.findByUsernameAndAgeGreaterThan("A", 15);
+        Assertions.assertThat(members.get(0).getAge()).isEqualTo(30);
+    }
+}
